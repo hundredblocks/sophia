c36df3363dd7e09c30b8e6352f5432ec38948a61
Emmanuel Ameisen <emmanuel.ameisen@gmail.com>
2016-11-28 22:21:20 -0800

    working review page parser and req fix

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,24 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# TODO iterate over pages
 def get_reviews_from_url(url):
-    doc = requests.get(url).text
+    pass
+
+
+def get_reviews_from_page(page_url):
+    doc = requests.get(page_url).text
     soup = BeautifulSoup(doc, 'html.parser')
     soup.find_all()
+    reviews = []
     reviews_dom = soup.findAll("div", {"class": "review review--with-sidebar"})
     for review_dom in reviews_dom:
         review_dic = {}
+
         user_name_dom = review_dom.find_next("a", {"class": "user-display-name"})
         review_dic["user_name"] = user_name_dom.string
-
-        user_loc_dom = review_dom.find_next("a", {"class": "user-location"})
+        user_loc_dom = review_dom.find_next("li", {"class": "user-location responsive-hidden-small"}).find_next("b")
         review_dic["user_location"] = user_loc_dom.string
+        user_friend_dom = review_dom.find_next("li", {"class": "friend-count"}).find_next("b")
+        review_dic["user_friends"] = user_friend_dom.text
+        user_reviewcount_dom = review_dom.find_next("li", {"class": "review-count"}).find_next("b")
+        review_dic["user_reviews"] = user_reviewcount_dom.text
 
-        user_friend_dom = review_dom.find_next("a", {"class": "friend-count"})
+        review_val_dom = review_dom.find_next("div", {"class": "review-content"})
+        review_text = review_val_dom.find_next("p")
+        review_dic["review_text"] = review_text.text
 
-        review_dic["user_friends"] = user_friend_dom.find_next("b").text
-
-        print(review_dic)
-
+        review_score = review_val_dom.find_next("div", {"class": "i-stars"})
+        title = review_score.attrs.get("title", "")
+        stars = title.split(" ")[0]
+        review_dic["review_score"] = stars
+        reviews.append(review_dic)
+    return reviews
 
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -6,21 +6,31 @@ def test_parsing():
         doc = f.read()
     soup = BeautifulSoup(doc, 'html.parser')
     soup.find_all()
+    reviews = []
     reviews_dom = soup.findAll("div", {"class": "review review--with-sidebar"})
     for review_dom in reviews_dom:
         review_dic = {}
+
         user_name_dom = review_dom.find_next("a", {"class": "user-display-name"})
         review_dic["user_name"] = user_name_dom.string
-
         user_loc_dom = review_dom.find_next("li", {"class": "user-location responsive-hidden-small"}).find_next("b")
         review_dic["user_location"] = user_loc_dom.string
-
         user_friend_dom = review_dom.find_next("li", {"class": "friend-count"}).find_next("b")
         review_dic["user_friends"] = user_friend_dom.text
-
         user_reviewcount_dom = review_dom.find_next("li", {"class": "review-count"}).find_next("b")
         review_dic["user_reviews"] = user_reviewcount_dom.text
 
+        review_val_dom = review_dom.find_next("div", {"class": "review-content"})
+        review_text = review_val_dom.find_next("p")
+        review_dic["review_text"] = review_text.text
+
+        review_score = review_val_dom.find_next("div", {"class": "i-stars"})
+        title = review_score.attrs.get("title", "")
+        stars = title.split(" ")[0]
+        review_dic["review_score"] = stars
+        reviews.append(review_dic)
+
+    print(reviews)
 
 if __name__=="__main__":
     test_parsing()
