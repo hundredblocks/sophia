2ca84fc192f38bb909c0004ac46eec5ca890f142
Ryan Mehta <rmehta@zipcar.com>
2016-11-29 19:28:16 -0800

    faster parsing

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,20 +1,43 @@
 import requests
+import json
+import math
+from functools import reduce
 from bs4 import BeautifulSoup
-from storage.csv_storage import store_review_list
+from multiprocessing import Pool
+from storage.csv_storage import store_review_list, get_review_list
 
 # TODO iterate over pages
 def get_reviews_from_url(url):
-    reviews = []
+    info = _get_review_info(url)
+    pages = [_get_review_url_for_page(url, page) for page in range(1, _get_number_of_pages(info.get('count', 0)))]
 
-    _reviews = _get_reviews_from_page(url)
-    reviews = reviews + _reviews
-    while len(_reviews) >= 20:
-        _reviews = _get_reviews_from_page(url+"?start=" + str(len(reviews)))
-        reviews = reviews + _reviews
+    with Pool(20) as p:
+        all_reviews_info = p.map(_get_review_info, pages)
+        all_reviews_info.append(info)
+        reviews = reduce(lambda acc, i: i.get('reviews', []) + acc, all_reviews_info, [])
 
+    store_review_list(reviews, url)
     return reviews
 
 # Private functions
+
+
+def _get_number_of_pages(review_count):
+    return math.ceil(review_count / 20)
+
+
+def _get_review_url_for_page(url, page_number):
+    return url + "?start=" + str(page_number*20)
+
+
+def _get_review_info(page_url):
+    print(page_url)
+    doc = requests.get(page_url).text
+    soup = BeautifulSoup(doc, 'html.parser')
+    j = soup.findAll("script", {"type": "application/ld+json"})[0].string.strip()
+    info = json.loads(j)
+    return {'count': info['aggregateRating']['reviewCount'], 'rating': info['aggregateRating']['ratingValue'], 'reviews': info['review']}
+
 
 def _get_reviews_from_page(page_url):
     print(page_url)
@@ -22,11 +45,15 @@ def _get_reviews_from_page(page_url):
     soup = BeautifulSoup(doc, 'html.parser')
     soup.find_all()
     reviews = []
+
+    j = soup.findAll("script", {"type": "application/ld+json"})[0].string.strip()
+    print(json.loads(j)['aggregateRating']['reviewCount'])
     reviews_dom = soup.findAll("div", {"class": "review review--with-sidebar"})
     for review_dom in reviews_dom:
         review_dic = _find_review(review_dom)
         reviews.append(review_dic)
     return reviews
+
 
 def _find_review(review_dom):
     review_dic = {}
