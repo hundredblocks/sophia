330d846c0c7802fdac3fc8fad8636687dc9eab1f
Ryan Mehta <rmehta@zipcar.com>
2016-11-30 20:57:18 -0800

    add a ui

--- routes/base.py
+++ routes/base.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from parsing.parsing import get_reviews_from_url
 from storage.csv_storage import store_review_list
 
 base_routes = Blueprint('requester', __name__ )
 
 
-@base_routes.route("/")
+@base_routes.route('/', methods=['GET'])
 def index():
-    return "This is the landing page"
+    return render_template('index.html')
 
 
-@base_routes.route("/results")
+@base_routes.route('/summary', methods=['POST'])
 def display_results():
-    url = "https://www.yelp.com/biz/philz-coffee-san-mateo"
+    url = request.form['reviewUrl']
     reviews = get_reviews_from_url(url)
     store_review_list(reviews, url)
-    return "These are the results"
+    return render_template('summary.html', rating=4, total=len(reviews), summary="None")
---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#summarize').click(function() {
+
+    })
+});
